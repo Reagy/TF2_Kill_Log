37ed92573b6691dd3dc47af9ad27722c5bb671b5
Sinclair47 <castor47@gmail.com>
2014-09-09 16:27:08 -0500

    cleanup sql

--- panel/sql/items.sql
+++ panel/sql/items.sql
@@ -1,30 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Sep 01, 2014 at 09:57 AM
--- Server version: 5.5.38-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `pit_stats`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `items`
---
 
 CREATE TABLE IF NOT EXISTS `items` (
   `index` int(6) NOT NULL DEFAULT '0',
@@ -34,16 +10,7 @@ CREATE TABLE IF NOT EXISTS `items` (
   `slot` varchar(9) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `items`
---
 ALTER TABLE `items`
  ADD PRIMARY KEY (`index`), ADD KEY `class` (`class`), ADD KEY `slot` (`slot`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ 

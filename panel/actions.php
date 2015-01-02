ea2bf40f49e40c04f50269a8369bd4c05868f109
Sinclair47 <Castor47@gmail.com>
2015-01-01 19:20:26 -0600

    Remove broken actions

--- panel/actions.php
+++ panel/actions.php
@@ -1,6 +1,7 @@
 <?php 
 
 $custkill = array('0' => 'None', '1' => 'Headshot', '2' => 'Backstab', '3' => 'Burning', '4' => 'Fix', '5' => 'Minigun', '6' => 'Suicide', '7' => 'Hadouken', '8' => 'Flare', '9' => 'Taunt', '10' => 'Taunt', '11' => 'Team Penetration', '12' => 'Plaer Penetration', '13' => 'Taunt', '14' => 'Penetration Headshot', '15' => 'Taunt', '16' => 'Telefrag', '17' => 'Burning Arrow', '18' => 'Flying Burn', '19' => 'Pumpkin Bomb', '20' => 'Decapitation', '21' => 'Taunt', '22' => 'Baseball', '23' => 'Charge Impact', '24' => 'Taunt', '25' => 'Air Sticky', '26' => 'Defensive Sticky', '27' => 'Pickaxe', '28' => 'Direct Hit', '29' => 'Taunt', '30' => 'Wrangled', '31' => 'Sticky', '32' => 'Revenge Crit', '33' => 'Taunt', '34' => 'Bleed', '35' => 'Golden', '36' => 'Carry Building', '37' => 'Combo', '38' => 'Taunt', '39' => 'Fish Kill', '40' => 'Hurt', '41' => 'Boss Decapitation', '42' => 'Explosion', '43' => 'Aegis', '44' => 'Flare Explode', '45' => 'Stomp', '46' => 'Plasma', '47' => 'Deflected Plasma', '48' => 'Plasma Gib', '49' => 'Practice Sticky', '50' => 'Eyeball', '51' => 'Hitman Headshot', '52' => 'Taunt', '53' => 'Flare', '54' => 'Cleaver', '55' => 'Crit Cleaver', '56' => 'Red Tape', '57' => 'Player Bomb', '58' => 'Merasmus Grenade', '59' => 'Merasmus Zap', '60' => 'Merasmus Decapitation', '61' => 'Cannonball Push', '70' => 'Fireball Spell','67' => 'Pumpkin MIRV Spell','73' => 'Ball O\'Bats Spell','71' => 'MONOCULUS Spell','66' => 'Skeletons Spell','68' => 'Meteor Shower Spell','74' => 'Minify Spell','69' => 'Tesla Bolt Spell','65' => 'Teleport Spell');
+
 //Include Config
 include "inc/config.php";
 
@@ -10,7 +11,7 @@ include 'inc/database.class.php';
 // Instantiate database.
 $database = new Database();
 
-$database->query('SELECT customkill, COUNT(customkill) AS total FROM smalllog WHERE customkill NOT IN(0,3,5,7,9,10,13,15,18,21,24,25,28,29,33,38,39,53,54,61)
+$database->query('SELECT customkill, COUNT(customkill) AS total FROM smalllog WHERE customkill NOT IN(0,3,5,7,9,10,13,15,18,21,24,25,28,29,33,38,39,53,54,61,75,72,82,98)
 	GROUP BY customkill ORDER BY total DESC');
 $log = $database->resultset();
 
